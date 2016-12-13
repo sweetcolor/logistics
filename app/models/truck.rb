0e49dafa715d7cb334a9e71e9a67ef1c8b711363
Yurii Zhytskyi <sweet3color@gmail.com>
2016-12-13 07:13:16 +0200

    fix

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,4 +1,12 @@
 class Truck < ApplicationRecord
   belongs_to :fuel
   has_one :delivery
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :number, presence: true
+  validates :mark, presence: true
+  validates :fuel_consumption, presence: true
+  validates :capacity_fuel, presence: true
+  validates :fuel_id, presence: true
 end
